98fbc44d4d9bd0e37564caba1a1de1804301309c
supakarn39 <supakarn39@hotmail.com>
2016-11-04 15:23:25 +0700

    Add files via upload

--- src/NORT/GameEngine.java
+++ src/NORT/GameEngine.java
@@ -1,11 +1,28 @@
 package NORT;
 
 import org.newdawn.slick.*;
+import java.util.ArrayList;
 
 public class GameEngine extends BasicGame{
-    float x = 400;
-    float y = 300;
-    char aswd = '\0';
+    ArrayList arrx1 = new ArrayList();
+    ArrayList arry1 = new ArrayList();
+    ArrayList arrx2 = new ArrayList();
+    ArrayList arry2 = new ArrayList();
+
+    float[][] arr = new float[800][600];
+
+    float x1 = 400;
+    float y1 = 300;
+    float x2 = 300;
+    float y2 = 400;
+
+    float x1old = x1;
+    float y1old = y1;
+    float x2old = x2;
+    float y2old = y2;
+
+    char aswd1 = 'w';
+    char aswd2 = 'w';
     Image car = null;
 
     public GameEngine(String title) {
@@ -17,47 +34,100 @@ public class GameEngine extends BasicGame{
 
     public void update(GameContainer contain, int i) throws SlickException {
         Input input = contain.getInput();
-        if (input.isKeyDown(Input.KEY_A)) {
-            aswd = 'a';
+
+        if (aswd1 != 'd' && input.isKeyDown(Input.KEY_A)) {
+            aswd1 = 'a';
         }
-        if (input.isKeyDown(Input.KEY_D)) {
-            aswd = 'd';
+        if (aswd1 != 'a' && input.isKeyDown(Input.KEY_D)) {
+            aswd1 = 'd';
         }
-        if (input.isKeyDown(Input.KEY_W)) {
-            aswd = 'w';
+        if (aswd1 != 's' && input.isKeyDown(Input.KEY_W)) {
+            aswd1 = 'w';
         }
-        if (input.isKeyDown(Input.KEY_S)) {
-            aswd = 's';
+        if (aswd1 != 'w' && input.isKeyDown(Input.KEY_S)) {
+            aswd1 = 's';
         }
-        if (x < 0 ) {
-            throw new SlickException("555");
+        if (aswd1 != 'd' && input.isKeyDown(Input.KEY_LEFT)) {
+            aswd2 = 'a';
         }
-        if (x > 800) {
-            throw new SlickException("555");
+        if (aswd1 != 'a' && input.isKeyDown(Input.KEY_RIGHT)) {
+            aswd2 = 'd';
         }
-        if (y < 0) {
-            throw new SlickException("555");
+        if (aswd1 != 's' && input.isKeyDown(Input.KEY_UP)) {
+            aswd2 = 'w';
         }
-        if (y > 600) {
-            throw new SlickException("555");
+        if (aswd1 != 'w' && input.isKeyDown(Input.KEY_DOWN)) {
+            aswd2 = 's';
         }
 
-        switch (aswd){
-            case 'a': x--;break;
-            case 'w': y--;break;
-            case 's': y++;break;
-            case 'd': x++;break;
+        switch (aswd1){
+            case 'a': x1--;break;
+            case 'w': y1--;break;
+            case 's': y1++;break;
+            case 'd': x1++;break;
+        }
+        switch (aswd2){
+            case 'a': x2--;break;
+            case 'w': y2--;break;
+            case 's': y2++;break;
+            case 'd': x2++;break;
+        }
+
+        for(int j = 1; j < arrx1.size(); j++) {
+            if (arrx1.size() >= 1) {
+                if (x1 == (float) arrx1.get(j) && y1 == (float) arry1.get(j)){
+                    throw new SlickException("555");
+                }
+                if (x1 == (float) arrx2.get(j) && y1 == (float) arry2.get(j)){
+                    throw new SlickException("555");
+                }
+            }
+
+            if(arrx2.size() >= 1) {
+                if (x2 == (float) arrx1.get(j) && y2 == (float) arry1.get(j)){
+                    throw new SlickException("555");
+                }
+                if (x2 == (float) arrx2.get(j) && y2 == (float) arry2.get(j)){
+                    throw new SlickException("555");
+                }
+            }
+        }
+
+        if(arrx1.size() == 0 ){
+            arrx1.add(x1);
+            arry1.add(y1);
+            arrx2.add(x2);
+            arry2.add(y2);
+        }
+
+        if (!(x1 == x1old && y1 == y1old )){
+            arrx1.add(x1);
+            arry1.add(y1);
+        }
+        if (!(x2 == x2old && y2 == y2old )) {
+            arrx2.add(x2);
+            arry2.add(y2);
         }
     }
 
     public void render(GameContainer contain, Graphics g) throws SlickException {
-        g.fillRect(x,y,5,5);
-        g.drawRect(x,y,5,5);
+        g.setColor(new Color(255,255,255));
+        for(int i = 0; i < arrx1.size(); i++){
+            g.fillRect((float) arrx1.get(i),(float) arry1.get(i),5,5);
+            g.drawRect((float) arrx1.get(i),(float) arry1.get(i),5,5);
+        }
+
+        g.setColor(new Color(255,0,0));
+        for(int i = 0; i < arrx2.size(); i++){
+            g.fillRect((float) arrx2.get(i),(float) arry2.get(i),5,5);
+            g.drawRect((float) arrx2.get(i),(float) arry2.get(i),5,5);
+        }
     }
 
     public static void main(String[] args) throws SlickException {
         AppGameContainer tron = new AppGameContainer(new GameEngine("NORT"));
 
+        tron.setTargetFrameRate(60);
         tron.setDisplayMode(800,600,false);
 
         tron.start();
